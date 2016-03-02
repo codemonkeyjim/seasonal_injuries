da794298fe1300611c124b0ccae3c3e57b12fd7a
Jim Pfleger <jpfleger@acm.org>
2016-03-01 22:39:59 -0700

    Better filter limits

--- ui.R
+++ ui.R
@@ -17,9 +17,15 @@ shinyUI(fluidPage(
       sliderInput('min_injuries',
                   "Minimum total injuries",
                   min = 0,
-                  max = 250000,
-                  value = 0,
-                  step = 10000)
+                  max = 150000,
+                  value = 10000,
+                  step = 5000),
+      helpText(
+        p('This tool will help you see the seasonality of product-based injuries. The underlying data is the number of US emergency room visits attributed to product classes in 2014.'),
+        p('The graphs to the right show the monthly injuries attributed to the named products.'),
+        p('Products are filtered based on the proportion of total injuries that occurred in the selected month. The top N largest proportions are then graphed. Thus products with about the same number of injuries every month are filtered out.'),
+        p('Use the ',em('Minimum total injuries'), 'slider to filter out products that caused few total injuries in 2014.')
+      )
     ),
     mainPanel(
       htmlOutput('top_n'),
