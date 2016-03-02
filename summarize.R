39ca1c34cc20e8a59548d93d24036ff567a16bca
Jim Pfleger <jpfleger@acm.org>
2016-03-01 19:25:25 -0700

    Better graph grid

--- summarize.R
+++ summarize.R
@@ -1,5 +1,4 @@
 library(dplyr)
-library(lubridate)
 library(ggplot2)
 
 load('data/injuries.Rdata')
@@ -7,11 +6,6 @@ load('data/injuries.Rdata')
 # Seasonality of injuries
 # For each month generate a top-n list
 # Graph the full year of the top-n members
-
-injuries <- injuries %>%
-  mutate(
-    month = month(trmt_date)
-  )
 
 # Determine the proportion of injuries per month per product code
 monthly <- injuries %>%
@@ -27,8 +21,8 @@ top_monthly <- monthly %>%
   group_by(month) %>%
   top_n(20, portion)
 
-mon = 1
-month_products <- top_monthly[top_monthly$month==mon,]$prod1
+mon = 'Jan'
+month_products <- subset(top_monthly, month %in% c(mon))$prod1
 
 graph_data = monthly %>%
   filter(
@@ -36,6 +30,6 @@ graph_data = monthly %>%
   )
 
 g <- ggplot(graph_data)
-g <- g + geom_line(aes(month, num))
+g <- g + geom_line(aes(month, num, group = 1))
 g <- g + facet_wrap(~prod1, scales = 'free_y')
 g
