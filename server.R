41946b614297b81d602ab288ee490d320f9ae8c3
Jim Pfleger <jpfleger@acm.org>
2016-03-01 19:53:54 -0700

    First working Shiny app (table of top N proportions)

--- server.R
+++ server.R
@@ -1,23 +1,26 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-# 
-# http://www.rstudio.com/shiny/
-#
-
-library(shiny)
+library(dplyr)
+library(ggplot2)
 
 shinyServer(function(input, output) {
-   
-  output$distPlot <- renderPlot({
-    
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2] 
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    
-  })
-  
+  load('data/injuries.Rdata')
+
+  # Seasonality of injuries
+  # For each month generate a top-n list
+  # Graph the full year of the top-n members
+
+  # Determine the proportion of injuries per month per product code
+  monthly <- injuries %>%
+    group_by(prod1, month) %>%
+    summarize(
+      num = round(sum(weight))
+    ) %>%
+    mutate(
+      portion = num / sum(num)
+    )
+
+  top_monthly <- monthly %>%
+    group_by(month) %>%
+    top_n(20, portion)
+
+  output$top_n <- renderTable(top_monthly)
 })
--- ui.R
+++ ui.R
@@ -1,28 +1,23 @@
+shinyUI(fluidPage(
+  titlePanel("Seasonality of Product Injuries"),
 
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-# 
-# http://www.rstudio.com/shiny/
-#
-
-library(shiny)
-
-shinyUI(pageWithSidebar(
-  
-  # Application title
-  headerPanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins
-  sidebarPanel(
-    sliderInput("bins",
-                "Number of bins:",
-                min = 1,
-                max = 50,
-                value = 30)
-  ),
-  
-  # Show a plot of the generated distribution
-  mainPanel(
-    plotOutput("distPlot")
+  sidebarLayout(
+    sidebarPanel(
+      numericInput('n',
+                   label = strong('Top N'),
+                   value = 10,
+                   step = 5,
+                   min = 5),
+      selectizeInput('month',
+                     label = strong('Month'),
+                     choices = list('Jan', 'Feb', 'Mar',
+                                    'Apr', 'May', 'Jun',
+                                    'Jul', 'Aug', 'Sep',
+                                    'Oct', 'Nov', 'Dec'))
+    ),
+    mainPanel(
+      htmlOutput('top_n')
+    )
   )
 ))
+
